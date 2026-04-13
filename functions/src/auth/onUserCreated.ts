c81753d67bd09b416016e6f366a3b53ad8d2f788
Oleksandr01001 <oleksandr.hushtyk.07@gmail.com>
2026-04-13 14:04:40 +0200

    refactor: modular functions structure

---
+++ functions/src/auth/onUserCreated.ts
@@ -0,0 +1,18 @@
+import * as admin from "firebase-admin";
+import {auth} from "firebase-functions/v1";
+import {FieldValue} from "firebase-admin/firestore";
+
+const db = admin.firestore();
+
+export const onUserCreated = auth.user().onCreate(async (user) => {
+  await db.collection("users").doc(user.uid).set({
+    uid: user.uid,
+    username: user.displayName ?? "",
+    email: user.email ?? "",
+    avatarUrl: null,
+    createdAt: FieldValue.serverTimestamp(),
+    lastSeen: FieldValue.serverTimestamp(),
+    isOnline: false,
+    fcmTokens: [],
+  });
+});
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,21 +1,5 @@
 import * as admin from "firebase-admin";
-import {auth} from "firebase-functions/v1";
-import {FieldValue} from "firebase-admin/firestore";
 
 admin.initializeApp();
 
-const db = admin.firestore();
-
-// crear usuario
-export const onUserCreated = auth.user().onCreate(async (user) => {
-  await db.collection("users").doc(user.uid).set({
-    uid: user.uid,
-    username: user.displayName ?? "",
-    email: user.email ?? "",
-    avatarUrl: null,
-    createdAt: FieldValue.serverTimestamp(),
-    lastSeen: FieldValue.serverTimestamp(),
-    isOnline: false,
-    fcmTokens: [],
-  });
-});
+export {onUserCreated} from "./auth/onUserCreated";
