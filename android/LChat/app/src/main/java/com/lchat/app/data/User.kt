3ed3041ac5fcbcd1e3035beeddb4a89212ca042d
Oleksandr01001 <oleksandr.hushtyk.07@gmail.com>
2026-05-20 19:09:18 +0200

    fix(android): isOnline always false
    - Added @PropertyName annotation to isOnline field
    - Firebase Firestore was not mapping isOnline due to JavaBeans
      is-prefix stripping convention

--- android/LChat/app/src/main/java/com/lchat/app/data/User.kt
+++ android/LChat/app/src/main/java/com/lchat/app/data/User.kt
@@ -1,10 +1,14 @@
 package com.lchat.app.data
 
+import com.google.firebase.firestore.PropertyName
+
 data class User(
     val uid: String = "",
     val username: String = "",
     val email: String = "",
     val avatarUrl: String? = null,
-    val isOnline: Boolean = false,
+    @get:PropertyName("isOnline")
+    @set:PropertyName("isOnline")
+    var isOnline: Boolean = false,
     val lastSeen: com.google.firebase.Timestamp? = null
 )
